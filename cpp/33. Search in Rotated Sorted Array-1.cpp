b0a65a41f84a8382cfc0990e218ffe8b7a0b1802
baicaihenxiao <baicaihenxiao@gmail.com>
2016-07-09 00:07:51 +0800

    2016-7-9 00:07:29/binary search

--- cpp/33. Search in Rotated Sorted Array-1.cpp
+++ cpp/33. Search in Rotated Sorted Array-1.cpp
@@ -23,6 +23,60 @@ You may assume no duplicate exists in the array.
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+        int begin = 0, end = nums.size();//range [begin, end);
+		int mid;
+		while (begin != end)
+		{
+			mid = (begin + end) / 2;// [begin, mid), mid, [mid + 1, end);
+			
+			/* 不能这样判断，因为target>mid时，有可能在左边也有可能在右边;
+			if (target == nums[mid])
+			{
+				return mid;
+			}
+			else if (target > nums[mid])
+			{
+				if (nums[mid] <= nums[end - 1])
+					begin = mid + 1;
+				else 
+			}
+			else if (target < nums[mid])
+			{
+				end = mid;
+			} */
+			if (target == nums[mid])
+			{
+				return mid;
+			}
+			
+			if (nums[begin] <= nums[mid])//[begin, mid] is ordered;
+			{
+				//ensure begin < target < mid, only target < mid is not ture;
+				if (target < nums[mid] && target >= nums[begin])//so we can ensure target is in the [begin, mid) if target < mid;
+				{
+					end = mid;
+				}
+				else//because this range is disordered, we can focus on the range with the else condition;
+				{
+					begin = mid + 1;
+				}
+			}
+			else//[mid, end) is ordered;
+			{
+				if (target > nums[mid] && target <= nums[end - 1])
+				{
+					begin = mid + 1;
+				}
+				else
+				{
+					end = mid;
+				}
+			}
+			
+			
+			
+		}
+		
+		return -1;
     }
 };
