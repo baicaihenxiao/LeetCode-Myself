da8b5e81bb19821542bf1d8608f573a02b551483
baicaihenxiao <baicaihenxiao@gmail.com>
2016-08-20 11:06:37 +0800

    unsolved/2016-8-20 11:06:30

---
+++ cpp/128.Longest Consecutive Sequence.cpp
@@ -0,0 +1,21 @@
+/*
+2016-8-20 11:05:38
+
+https://leetcode.com/problems/longest-consecutive-sequence/
+
+128. Longest Consecutive Sequence  QuestionEditorial Solution  My Submissions
+Total Accepted: 73848
+Total Submissions: 220055
+Difficulty: Hard
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+For example,
+Given [100, 4, 200, 1, 3, 2],
+The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+
+Your algorithm should run in O(n) complexity.
+
+
+*/
+
+//unsolved
