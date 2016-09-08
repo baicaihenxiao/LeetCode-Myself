d7f07920015025d06c7ab7559233661b63fae44c
baicaihenxiao <baicaihenxiao@gmail.com>
2016-09-09 00:05:41 +0800

    solved

--- cpp/48. Rotate Image.cpp
+++ cpp/48. Rotate Image.cpp
@@ -17,5 +17,45 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
+		int n = matrix.size();
+		
+		if (n <= 1)
+		{
+			return;
+		}
+			
+		int tmp = 0;
+		int jRange = 0;
+		
+		if (n % 2 == 1)
+		{
+		    jRange = n / 2 + 1;
+		}
+		else
+		{
+		    jRange = n / 2;
+		}
+		
+		for (int i = 0; i < n / 2; ++ i)
+		{
+			for (int j = 0; j < jRange; ++ j)
+			{
+				//clockwise: matrix[i][j]  matrix[j][n - i - 1]  matrix[n - i - 1][n - j - 1]  matrix[n - j - 1][i] 
+				tmp = matrix[i][j];
+				matrix[i][j] = matrix[n - j - 1][i];
+				matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+				matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+				matrix[j][n - i - 1] = tmp;
+				// matrix[j][n - i - 1] = matrix[i][j];
+				// matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+				// matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+				
+				
+				
+			}
+		}
+		
+		return;
+		
     }
 };
