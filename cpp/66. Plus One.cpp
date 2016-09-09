9cfa17b3887155b28db8803a8066bc1c3fc9d0ef
baicaihenxiao <baicaihenxiao@gmail.com>
2016-09-09 23:38:56 +0800

    solved

--- cpp/66. Plus One.cpp
+++ cpp/66. Plus One.cpp
@@ -14,7 +14,28 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         
-		cout << digits.data();
+		bool carry = false;
 		
+		for (int i = digits.size() - 1; i >= 0; -- i)
+		{
+			if (digits[i] != 9)
+			{
+				++ digits[i];
+				carry = false;
+				break;
+			}
+			else
+			{
+				digits[i] = 0;
+				carry = true;
+			}
+		}
+		
+		if (carry)
+		{
+			digits.insert(digits.begin(), 1);
+		}
+		
+		return digits;
     }
 };
