7dd9d442e449fe94164b970e67c172294b89e11e
baicaihenxiao <baicaihenxiao@gmail.com>
2016-09-09 12:28:16 +0800

    unsolved

---
+++ cpp/66. Plus One.cpp
@@ -0,0 +1,20 @@
+﻿/*
+2016-9-9 12:19:38
+
+https://leetcode.com/problems/plus-one/
+
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+*/
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+		cout << digits.data();
+		
+    }
+};
