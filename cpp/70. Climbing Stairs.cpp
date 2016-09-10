09214a3f45ebc0e0c9e085e244137fd7c096950f
baicaihenxiao <baicaihenxiao@gmail.com>
2016-09-10 16:43:33 +0800

    solved

--- cpp/70. Climbing Stairs.cpp
+++ cpp/70. Climbing Stairs.cpp
@@ -13,6 +13,17 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 class Solution {
 public:
     int climbStairs(int n) {
-        
+        vector<int> vec(n + 1, 0);
+		
+		vec[1] = 1;
+		vec[2] = 2;
+		
+		for (int i = 3; i <= n; ++ i)
+		{
+			vec[i] = vec[i - 1] + vec[i - 2];
+		}
+		
+		return vec[n];
+		
     }
 };
