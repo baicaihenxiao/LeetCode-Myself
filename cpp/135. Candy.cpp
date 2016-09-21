a6b4c395073caa023e6f9bce633af7e0ee662926
baicaihenxiao <baicaihenxiao@gmail.com>
2016-09-21 13:06:42 +0800

    solved

--- cpp/135. Candy.cpp
+++ cpp/135. Candy.cpp
@@ -13,10 +13,39 @@ What is the minimum candies you must give?
 
 */
 
+#include <algrithm>
 
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         
+		int res = 0;
+		vector<int> candy(ratings.size(), 1);
+		//candy.reserve(ratings.size());
+		
+		for (int i = 1; i < ratings.size(); ++ i)
+		{
+			if (ratings[i] > ratings[i - 1])
+			{
+				candy[i] = candy[i - 1] + 1;
+			}
+		}
+		
+		for (int i = ratings.size() - 2; i >= 0; -- i)
+		{
+			if (ratings[i] > ratings[i + 1])
+			{
+				candy[i] = std::max(candy[i + 1] + 1, candy[i]);
+			}
+			
+		}
+		
+		for (int i = 0; i < ratings.size(); ++ i)
+		{
+			res += candy[i];
+		}
+		
+		return res;
+		
     }
 };
