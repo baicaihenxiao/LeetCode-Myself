4d77792ba93f87a1fb658530e0c403c2bedd4c5e
baicaihenxiao <baicaihenxiao@gmail.com>
2016-09-24 12:14:57 +0800

    solved/use unordered_map

--- cpp/128.Longest Consecutive Sequence.cpp
+++ cpp/128.Longest Consecutive Sequence.cpp
@@ -18,4 +18,55 @@ Your algorithm should run in O(n) complexity.
 
 */
 
-//unsolved
+
+#include <unordered_map>
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+		unordered_map<int, bool> m;
+		
+		int longest = 0;
+		int tmpLong = 0;
+		
+		for (auto i: nums)
+		{
+			m[i] = false;
+		}
+		
+		for (auto i: nums)
+		{
+			if (m[i])
+			{
+				continue;
+			}
+			else
+			{
+				m[i] = true;
+				
+				tmpLong = 1;
+				
+				for (int j = i + 1; m.find(j) != m.end(); ++ j)
+				{
+					m[j] = true;
+					++ tmpLong;
+				}
+				
+				for (int j = i - 1; m.find(j) != m.end(); -- j)
+				{
+					m[j] = true;
+					++ tmpLong;
+				}
+				
+				if (tmpLong > longest)
+					longest = tmpLong;
+				
+			}
+		}
+		
+		return longest;
+
+		
+    }
+};
