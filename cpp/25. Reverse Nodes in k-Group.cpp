b8714d8045f2b374c6ad778732844eb45bd0bdfb
baicaihenxiao <baicaihenxiao@gmail.com>
2016-10-03 21:09:27 +0800

    solved

--- cpp/25. Reverse Nodes in k-Group.cpp
+++ cpp/25. Reverse Nodes in k-Group.cpp
@@ -1,3 +1,25 @@
+/*
+2016-10-3 21:04:32
+
+https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+*/
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -22,6 +44,7 @@ public:
 		
 		ListNode* begin = phead;
 		ListNode* end;
+		ListNode* tmp;
 		
 		int i = 0;
 		
@@ -32,7 +55,7 @@ public:
 			
 			for (i = 0; i < k; ++ i)
 			{
-				if (end->next == nullptr)//有问题，想想先end->next还是先这句.
+				if (end->next == nullptr)
 				{
 					break;
 				}
@@ -40,9 +63,31 @@ public:
 				end = end->next;
 			}
 			
-			
+			if (k == i)
+			{
+				ListNode* iter = begin->next;
+				
+				for (i = 1; i < k; ++ i)
+				{
+					tmp = begin->next;
+					begin->next = iter->next;
+					iter->next = iter->next->next;
+					begin->next->next = tmp;
+					
+				}
+				
+				begin = iter;
+			}
+			else
+			{
+				break;
+			}
 			
 		}
+		
+		
+		return phead->next;
+
 		
 		
     }
