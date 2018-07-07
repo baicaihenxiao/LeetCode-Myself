159170c4e4c373103bc661c5a0b447995b2eb977
baicaihenxiao <baicaihenxiao@gmail.com>
2018-07-07 23:56:14 +0800

    unsolved

---
+++ cpp/32. Longest Valid Parentheses.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/longest-valid-parentheses/#/description
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        Stack<Boolean> s = new Stack<>();
+        
+    }
+};
---
+++ java/7. Reverse Integer.java
@@ -0,0 +1,8 @@
+// 2018-07-01 03:00:42
+// https://leetcode.com/problems/reverse-integer/description/
+
+class Solution {
+    public int reverse(int x) {
+        
+    }
+}
