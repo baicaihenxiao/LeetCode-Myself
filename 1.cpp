4e0aa7a940b8e8b0bc2655d0411a725be8c1a97e
Cody <baicaihenxiao@gmail.com>
2019-05-04 11:47:52 +0800

    1.cpp add c++ model

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,19 @@
-#include
-asdfasdf
-asdfasdf
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <deque>
+
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    
+    return 0;
+}
