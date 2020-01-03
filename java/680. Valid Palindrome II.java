edce438225210b7e53cd1cdf5d3291ac957f9b36
baicaihenxiao <baicaihenxiao@gmail.com>
2020-01-03 15:46:37 +0800

    solved java 680. Valid Palindrome II.java

---
+++ java/680. Valid Palindrome II.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        return isPalindromeWithDel(s, true);
+    }
+    
+    public boolean isPalindromeWithDel(String s, boolean flag) {
+
+        if (s == null || s.isEmpty()) {
+            return true;
+        }
+
+        int i = 0, j = s.length() - 1;
+
+        while (i <= j) {
+
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                ++ i;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(s.charAt(j))) {
+                -- j;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                if (flag) {
+                    return isPalindromeWithDel(s.substring(i, j), false) || isPalindromeWithDel(s.substring(i + 1, j + 1), false);
+                } else {
+                    return false;
+                }
+            }
+
+            ++ i;
+            -- j;
+        }
+
+        return true;
+    }
+}
