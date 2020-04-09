6d5bc62be1bdaa08a3ae6549dbf36e6fd98fc239
Cody <baicaihenxiao@gmail.com>
2020-04-10 00:20:55 +0800

    feat: add test for 3. Longest Substring Without Repeating Characters.java
    测试运行需要class是public的，而public的类需要和文件名一致，所以要取合法的文件名

---
+++ java/src/test/java/club/cser/leetcode/LongestSubstringWithoutTest.java
@@ -0,0 +1,22 @@
+package club.cser.leetcode;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+//3. Longest Substring Without Repeating Characters.java
+public class LongestSubstringWithoutTest {
+
+
+    static final Logger LOGGER = LogManager.getLogger(LongestSubstringWithoutTest.class);
+
+
+    @Test
+    public void sss() {
+        LongestSubstringWithout t = new LongestSubstringWithout();
+        Assert.assertEquals(3, t.lengthOfLongestSubstring("pwwkew"));;
+
+    }
+
+}
