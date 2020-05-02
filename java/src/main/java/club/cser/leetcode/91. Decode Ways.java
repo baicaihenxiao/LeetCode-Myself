3fe79918daad801c905c382818cbdc06282ed5b8
Cody <baicaihenxiao@gmail.com>
2020-05-02 22:42:48 +0800

    solved java 91  +1

--- java/src/main/java/club/cser/leetcode/91. Decode Ways.java
+++ java/src/main/java/club/cser/leetcode/91. Decode Ways.java
@@ -1,6 +1,7 @@
 package club.cser.leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class DecodeWays {
@@ -38,5 +39,28 @@ class DecodeWays {
 
         return dp.get(s.length());
     }
-    
+    public int simplifiedNumDecodings(String s) {
+        if (s.charAt(0) == '0')
+            return 0;
+
+        List<Integer> dp = new ArrayList<Integer>(Collections.nCopies(s.length() + 1, 0));
+
+        dp.set(0, 1);
+        dp.set(1, 1);
+
+        for (int i = 1; i < s.length(); ++ i) {
+            int twoDigit = (s.charAt(i -1) - '0') * 10 + s.charAt(i) - '0';
+            int oneDigit =  s.charAt(i) - '0';
+
+            if (twoDigit >= 10 && twoDigit <=26)
+                dp.set(i + 1, dp.get(i - 1));
+
+            if (oneDigit > 0 && oneDigit < 10)
+                dp.set(i + 1, dp.get(i) + dp.get(i + 1));
+        }
+        System.out.println(s);
+        System.out.println(dp);
+
+        return dp.get(s.length());
+    }
 }
--- java/src/test/java/club/cser/leetcode/DecodeWaysTest.java
+++ java/src/test/java/club/cser/leetcode/DecodeWaysTest.java
@@ -30,4 +30,24 @@ public class DecodeWaysTest {
         Assert.assertEquals(1 ,d.numDecodings("10"));
         Assert.assertEquals(0 ,d.numDecodings("100"));
     }
+
+
+    @Test
+    public void normalSimplifiedTest() {
+        Assert.assertEquals(2, d.simplifiedNumDecodings("12"));
+        Assert.assertEquals(1, d.simplifiedNumDecodings("2"));
+        Assert.assertEquals(1, d.simplifiedNumDecodings("52"));
+        Assert.assertEquals(3, d.simplifiedNumDecodings("226"));
+    }
+
+    @Test
+    public void extremePositiveSimplifiedTest() {
+        Assert.assertEquals(0 ,d.simplifiedNumDecodings("0"));
+        Assert.assertEquals(0 ,d.simplifiedNumDecodings("05"));
+        Assert.assertEquals(0 ,d.simplifiedNumDecodings("00"));
+        Assert.assertEquals(0 ,d.simplifiedNumDecodings("50"));
+        Assert.assertEquals(1 ,d.simplifiedNumDecodings("10"));
+        Assert.assertEquals(0 ,d.simplifiedNumDecodings("100"));
+        Assert.assertEquals(0 ,d.simplifiedNumDecodings("10058"));
+    }
 }
