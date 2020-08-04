babf0bf248915bb2b7cfd683269a70b0f273c470
Cody <baicaihenxiao@gmail.com>
2020-08-04 13:16:48 +0800

    solved java 102 107

---
+++ java/src/main/java/club/cser/leetcode/102. Binary Tree Level Order Traversal.java
@@ -0,0 +1,42 @@
+package club.cser.leetcode;
+
+import java.util.*;
+
+class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        int numOfCurRow = 0;
+
+
+        if (root != null) {
+            res.add(Arrays.asList(root.val));
+            dq.addLast(root);
+            numOfCurRow = 1;
+        }
+
+        while (!dq.isEmpty()) {
+            List<Integer> nextRow = new ArrayList<>();
+            int numOfNextRow = 0;
+            for (int i = 0; i < numOfCurRow; i++) {
+                TreeNode node = dq.pollFirst();
+                if (node.left != null) {
+                    nextRow.add(node.left.val);
+                    dq.addLast(node.left);
+                    ++ numOfNextRow;
+
+                }
+                if (node.right != null) {
+                    nextRow.add(node.right.val);
+                    dq.addLast(node.right);
+                    ++ numOfNextRow;
+                }
+            }
+            numOfCurRow = numOfNextRow;
+            if (!nextRow.isEmpty())
+                res.add(nextRow);
+        }
+
+        return res;
+    }
+}
