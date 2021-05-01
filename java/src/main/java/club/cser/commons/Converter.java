5e8f5f1d0cb0e6c749686d7c781dbf82875be2f8
Cody <baicaihenxiao@gmail.com>
2021-05-01 15:14:08 +0800

    feat<common>: add jackson

--- java/src/main/java/club/cser/commons/Converter.java
+++ java/src/main/java/club/cser/commons/Converter.java
@@ -1,10 +1,15 @@
 package club.cser.commons;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Converter {
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     public static String listToString(List<? extends Object> list) {
         return list.stream().map(Object::toString).collect(Collectors.joining(", "));
@@ -16,5 +21,19 @@ public class Converter {
 
     public static String charToString(char[][] chs) {
         return Arrays.stream(chs).map(Arrays::toString).reduce((a, b) -> a + b).get();
+    }
+
+    public static String charToString(boolean[][] chs) {
+        return Arrays.stream(chs).map(Arrays::toString).reduce((a, b) -> a + "\n" + b).get();
+    }
+
+    public static List<List<String>> JSONTo2DList(String JSONString) {
+        List<List<String>> res = new ArrayList<>();
+        try {
+            res = objectMapper.readValue(JSONString, List.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 }
